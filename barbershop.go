7bad5e45d038e7c5c7543df45736fa116306fbaf
Vatt Sopheak <vattcarter@gmail.com>
2023-03-10 16:47:23 +0700

    Implement add barber

---
+++ barbershop.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"time"
+
+	"github.com/fatih/color"
+)
+
+type BarberShop struct {
+	ShopCapacity    int
+	HaircutDuration time.Duration
+	NumberOfBarbers int
+	BarbersDoneChan chan bool
+	ClientsChan     chan string
+	Open            bool
+}
+
+func (shop *BarberShop) addBarber(barber string) {
+	shop.NumberOfBarbers++
+
+	go func() {
+		isSleeping := false
+		color.Yellow("%s goes to the waiting room to check for clients", barber)
+
+		for {
+			// if there are no clients, the barber goes to sleep
+			if len(shop.ClientsChan) == 0 {
+				color.Yellow("There is nothing to do, so %s takes a nap.", barber)
+				isSleeping = true
+			}
+
+			client, shopOpen := <-shop.ClientsChan
+
+			if shopOpen {
+				if isSleeping {
+					color.Yellow("%s wakes %s up.", client, barber)
+					isSleeping = false
+				}
+				// cut hair
+				shop.cutHair(barber, client)
+			} else {
+				// shop is closed, so send the barber home and close this goroutine
+				shop.sendBarberHome(barber)
+				return
+			}
+		}
+	}()
+}
+
+func (shop *BarberShop) cutHair(barber, client string) {
+	color.Green("%s is cutting %s's hair.", barber, client)
+	time.Sleep(shop.HaircutDuration)
+	color.Green("%s is finished cutting %s's hair.", barber, client)
+}
+
+func (shop *BarberShop) sendBarberHome(barber string) {
+	color.Cyan("%s is going home.", barber)
+	shop.BarbersDoneChan <- true
+}
--- main.go
+++ main.go
@@ -22,18 +22,45 @@
 
 package main
 
+import (
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
 // variables
+var seatingCapacity = 10
+var arrivalRate = 100
+var cutDuration = 1000 * time.Microsecond
+var timeOpen = 10 * time.Second
 
 func main() {
 	// seed our random number generator
+	rand.Seed(time.Now().UnixNano())
 
 	// print welcome message
+	color.Yellow("The Sleeping Barber Problem")
+	color.Yellow("---------------------------")
 
 	// create channels if we need any
+	clientChan := make(chan string, seatingCapacity)
+	doneChan := make(chan bool)
 
 	// create the barbershop
+	shop := BarberShop{
+		ShopCapacity:    seatingCapacity,
+		HaircutDuration: cutDuration,
+		NumberOfBarbers: 0,
+		ClientsChan:     clientChan,
+		BarbersDoneChan: doneChan,
+		Open:            true,
+	}
+
+	color.Green("The shop is open for the day!")
 
 	// add barbers
+	shop.addBarber("Frank")
 
 	// start the barbershop as a goroutine
 
@@ -41,4 +68,5 @@ func main() {
 
 	// block until the barbershop is closed
 
+	time.Sleep(5 * time.Second)
 }
